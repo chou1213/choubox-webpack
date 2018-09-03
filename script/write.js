ebb58c524c12f7106a61cab38ef94d120183a10d
chou1213 <joan.zhou2@gmail.com>
2018-09-03 17:56:32 +0800

    '整理文件'

--- script/write.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const projectname = 'quickStock';
-let template = 'module.exports={filename:"' + projectname + '"}';
-fs.writeFileSync(path.resolve(__dirname, './project.config.js'), template, 'utf8');
