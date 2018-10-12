262239990c655ea6b3220524e451743b1bd5f4ef
chou1213 <joan.zhou2@gmail.com>
2018-10-12 19:54:13 +0800

    '导出的js文件删除hash值'

--- script/webpack.config.js
+++ script/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     output: Object.assign({
         path: projectConfig.output.path || paths.distPath, //输出路径
-        filename: 'static/js/[name].[hash:7].js', //入口文件输出的文件名
+        filename: 'static/js/[name].js', //入口文件输出的文件名
         publicPath: '' //“开发模式”下静态资源的前缀
     }, env === 'production' ? {
         // chunkFilename: 'static/js/[name].js', //非入口文件输出的文件名
